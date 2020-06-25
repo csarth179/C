6607e621b2666d316fd1826d04bf7cd4b9527405
Sarthak Chopra <sarthak.c96@gmail.com>
2020-06-25 02:51:18 -0500

    Create string_array.c

---
+++ Procedural/Pointers/string_array.c
@@ -0,0 +1,31 @@
+//
+//  string_array.c
+//  String_Array
+//
+//  Created by Sarthak Chopra on 6/25/20.
+//  Copyright © 2020 Sarthak Chopra. All rights reserved.
+//
+
+#include<stdio.h>
+#include<string.h>
+
+int factorial(int );
+ 
+int main()
+{
+    int i = 1;
+ 
+    char *sports[] =
+    {               "golf",
+                    "hockey",
+                    "football",
+                    "cricket",
+                    "shooting"
+    };
+    
+    for(i = 0; i < 5; i++) {
+        printf("String = %s\n", sports[i]);
+        printf("Address of string literal = %u\n", sports[i]);
+    }
+    return 0;
+}
