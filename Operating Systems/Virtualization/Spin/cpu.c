d5595fa491c0b46ee83a37696b272350eac24122
Sarthak Chopra <sarthak.c96@gmail.com>
2020-06-25 02:51:13 -0500

    Create cpu.c

---
+++ Operating Systems/Virtualization/Spin/cpu.c
@@ -0,0 +1,26 @@
+//
+//  cpu.c
+//  Spin
+//
+//  Created by Sarthak Chopra on 6/25/20.
+//  Copyright © 2020 Sarthak Chopra. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+    fprintf(stderr, "usage: cpu <string>\n");
+    exit(1);
+    }
+    
+    char *str = argv[1];
+
+    while (1) {
+        printf("%s\n", str);
+        Spin(1);
+    }
+    return 0;
+}
