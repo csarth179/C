742639ee719e7d714d3eb0aee7c672de8a37b66e
Sarthak Chopra <sarthak.c96@gmail.com>
2020-06-27 20:39:26 -0500

    Create fork.c

---
+++ Operating Systems/Process/fork.c
@@ -0,0 +1,30 @@
+//
+//  fork.c
+//
+//  Created by Sarthak Chopra on 6/25/20.
+//  Copyright © 2020 Sarthak Chopra. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int
+main(int argc, char *argv[])
+{
+    printf("hello world (pid:%d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // child (new process)
+        printf("hello, I am child (pid:%d)\n", (int) getpid());
+    } else {
+        // parent goes down this path (original process)
+        printf("hello, I am parent of %d (pid:%d)\n",
+	       rc, (int) getpid());
+    }
+    return 0;
+}
